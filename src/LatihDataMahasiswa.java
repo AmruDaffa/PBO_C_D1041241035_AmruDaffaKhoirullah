53affd5a567e775a3021de519c6edf3b6033b750
unknown <amrudaffa83@gmail.com>
2026-02-13 23:18:02 +0700

    test

--- src/LatihDataMahasiswa.java
+++ src/LatihDataMahasiswa.java
@@ -1,11 +1,18 @@
 public class LatihDataMahasiswa {
     public static void main(String[] args) {
+        //Deklarasi NIM dengan String 
         String NIM = "D1041241035";
+        //Deklarasi Nama dengan String 
         String Nama = "Amru Daffa Khoirulllah";
+        //Deklarasi Nama dengan String 
         char Kelas = 'A';
+        //Deklarasi Kelas dengan Char 
         boolean Status = true;
+        //Deklarasi IPK dengan Double 
         double IPK = 3.94;
+        //Deklarasi SKS dengan int 
         int SKS = 66;
+        // Menampilkan Data Mahasiswa
         System.out.println("=== DATA MAHASISWA ===");
         System.out.println("NIM             : " + NIM);
         System.out.println("Nama            : " + Nama);
--- src/LatihPerhitungan.java
+++ src/LatihPerhitungan.java
@@ -1,17 +1,21 @@
 public class LatihPerhitungan {
     public static void main(String[] args) {
+        //Deklarasi Saldo Awal
         int Saldo_Awal = 1000000;
+        //Deklarasi Setoran
         int Setoran = 500000;
+        //Deklarasi Penarikan
         int Penarikan = 250000;
+        //Deklarasi Saldo Setelah Setor dengan Menambahkan Saldo Awala dengan Setoran
         int Saldo_Setelah_Setor = Saldo_Awal + Setoran;
+        //Deklarsi Saldo Akhir dengan Mengurangkan Saldo Setelah Setor dengan Penarikan
         int saldo_akhir = Saldo_Setelah_Setor - Penarikan;
+        //Output 
         System.out.println("=== SIMULASI TRANSAKSI BANK ===");
         System.out.println("Saldo Awal              : " + Saldo_Awal);
         System.out.println("Setoran                 : " + Setoran);
         System.out.println("Saldo Setelah Setor     : " + Saldo_Setelah_Setor);
         System.out.println("Penarikan               : " + Penarikan);
         System.out.println("Saldo Akhir             : " + saldo_akhir);
-
-
     }
 }
